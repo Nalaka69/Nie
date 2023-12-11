3c42f9c9b779b9bfd693aa9c9d861acb42059818
Kusal Wijekoon <79883649+iamkw97@users.noreply.github.com>
2023-12-11 12:32:33 +0530

    version 8

--- alphau/alphau__.sql
+++ alphau/alphau__.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2023 at 07:39 AM
+-- Generation Time: Dec 11, 2023 at 08:01 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
